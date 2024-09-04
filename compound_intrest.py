522904bdcd90fcc422c62d98f3dd3aa26872cba8
karthiknight30@gmail.com <karthiknight30@gmail.com>
2024-09-04 16:25:05 +0530

    added compount intrest code

---
+++ compound_intrest.py
@@ -0,0 +1,21 @@
+principle=0
+rate=0
+time=0
+
+while principle <=0:
+    principle= float(input("Enter pinciple amount (Rs):"))
+    if principle<=0:
+        print("principle amount can't be less than or equal to zero")
+
+while rate <=0:
+    rate= float(input("Enter intrest rate (%):"))
+    if principle<=0:
+        print("Rate  can't be less than or equal to zero")
+
+while time <=0:
+    time= int(input("Enter time (Y):"))
+    if principle<=0:
+        print("Time can't be less than or equal to zero")
+
+total=principle*pow((1+rate/100), time)
+print(f"Baalance after {time} year/s :{total:.2f}.RS")
